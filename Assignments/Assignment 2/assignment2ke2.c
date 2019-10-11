d234f07dc6b5f8944d755f100c27233c4f18bd55
Andrew Duong <andrewduong77@yahoo.com>
2019-10-11 17:59:22 -0500

    still working on assignment 2

---
+++ Assignments/Assignment 2/assignment2ke2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char* getfield(char* line, int num)
+{
+    const char* tok;
+    for (tok = strtok(line, ",");
+            tok && *tok;
+            tok = strtok(NULL, ",\n"))
+    {
+        if (!--num)
+            return tok;
+    }
+    return NULL;
+}
+
+int main()
+{int a =1;
+	int lat[14056];
+    FILE* stream = fopen("all_month.csv", "r");
+
+    char line[1024];
+    while (fgets(line, 1024, stream))
+    {
+        char* tmp = strdup(line);
+        printf("%d would be %s\n", a, getfield(tmp, 2));
+        // NOTE strtok clobbers tmp
+	
+        free(tmp);
+a++;
+    }
+}
