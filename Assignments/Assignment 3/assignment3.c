c1087f899eff5ab71fd2561658926f52372ec534
Andrew Duong <andrewduong77@yahoo.com>
2019-11-28 20:25:39 -0600

    completed assignment, tested, and ran

--- Assignments/Assignment 3/assignment3.c
+++ Assignments/Assignment 3/assignment3.c
@@ -7,137 +7,101 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define BUFFERSIZE 1024
-#define COPYMORE 0644
-
-void oops(char *, char *);
-int copyDir(char *src, char *dest);
-int copyFiles(char *src, char *dest);
-int dostat(char *filename);
-int mode_isReg(struct stat info);
-int main(int ac, char *av[])
+void error(char * message1, char * message2)
 {
-    if (ac != 3)
-    {
-        fprintf(stderr, "usage: %s source destination\n", *av);
-        exit(1);
-    }
-    char *src = av[1];
-    char *dest = av[2];
-    if (src[0] != '/' && dest[0] != '/')
-    {
-        copyFiles(src, dest);
-    }
-    else if (src[0] != '/' && dest[0] == '/')
-    {
-        int i;
-        for (i = 1; i <= strlen(dest); i++)
-        {
-            dest[(i - 1)] = dest[i];
-        }
-        strcat(dest, "/");
-        strcat(dest, src);
-        copyFiles(src, dest);
-    }
-    else if (src[0] == '/' && dest[0] == '/')
-    {
-        int i;
-        for (i = 1; i <= strlen(dest); i++)
-        {
-            dest[(i - 1)] = dest[i];
-        }
-        for (i = 1; i <= strlen(src); i++)
-        {
-            src[(i - 1)] = src[i];
-        }
-        copyDir(src, dest);
-    }
-    else
-    {
-        fprintf(stderr, "usage: cp1 source destination\n");
-        exit(1);
-    }
+    fprintf(stderr, "Error: %s ", message1);
+    perror(message2);
+    exit(1);
 }
-int copyDir(char *source, char *destination)
+int make_stat(char *filename)
 {
-    DIR *dir_ptr = NULL;
-    struct dirent *direntp;
-    char tempDest[strlen(destination) + 1];
-    char tempSrc[strlen(source) + 1];
+    struct stat fileinfo;
+    if(stat(filename, &fileinfo) >= 0)
+        if(S_ISREG(fileinfo.st_mode))
+            return 1;
+    return 0;
+}
+int files_copy(char * source, char * destination)
+{
+    int input_directory, output_directory, count;
+    char buffer[512];
+    if((input_directory = open(source, O_RDONLY)) == -1)
+        error("Cannot open ", source);
+    if((output_directory = creat(destination, 512)) == -1)
+        error("Cannot creat ", destination);
+    while ((count = read(input_directory, buffer, 512)) > 0)
+    {
+        if(write(output_directory, buffer, count) != count)
+            error("Write error to ", destination);
+        if(count == -1)
+            error("Read error from ", source);
+    }
+    if(close(input_directory) == -1 || close(output_directory) == -1)
+        error("Error closing files", "");
+    return 1;
+}
+int directory_copy(char * source, char * destination)
+{
+    DIR * dir = NULL;
+    struct dirent * dirent_my;
+    char temp_destination[strlen(destination) + 1];
+    char temp_source[strlen(source) + 1];
     strcat(destination, "/");
     strcat(source, "/");
-    strcpy(tempDest, destination);
-    strcpy(tempSrc, source);
-    //char *fileN;
-    struct stat fileinfo;
-    if ((dir_ptr = opendir(source)) == NULL)
+    strcpy(temp_destination, destination);
+    strcpy(temp_source, source);
+    if((dir = opendir(source)) == NULL)
     {
         fprintf(stderr, "cp1: cannot open %s for copying\n", source);
         return 0;
     }
     else
     {
-        while ((direntp = readdir(dir_ptr)))
-        {
-            //printf("direntp DNAME is %s\n", direntp->d_name);
-            //fileN = direntp->d_name;
-            //printf("File name before Reg Check is %s\n", fileN);
-            if (dostat(direntp->d_name))
+        while((dirent_my = readdir(dir)))
+            if(make_stat(dirent_my->d_name))
             {
-                strcat(tempDest, direntp->d_name);
-                strcat(tempSrc, direntp->d_name);
-                copyFiles(tempSrc, tempDest);
-                strcpy(tempDest, destination);
-                strcpy(tempSrc, source);
+                strcat(temp_destination, dirent_my->d_name);
+                strcat(temp_source, dirent_my->d_name);
+                files_copy(temp_source, temp_destination);
+                strcpy(temp_destination, destination);
+                strcpy(temp_source, source);
             }
-        }
-        closedir(dir_ptr);
+        closedir(dir);
         return 1;
     }
 }
-int dostat(char *filename)
+int main(int argc, char *argv[])
 {
-    struct stat fileInfo;
-    //printf("Next File %s\n", filename);
-    if (stat(filename, &fileInfo) >= 0)
-        if (S_ISREG(fileInfo.st_mode))
-            return 1;
-    return 0;
-}
-int copyFiles(char *source, char *destination)
-{
-    int in_fd, out_fd, n_chars;
-    char buf[BUFFERSIZE];
-    /* open files */
-    if ((in_fd = open(source, O_RDONLY)) == -1)
+    if(argc != 3)
     {
-        oops("Cannot open ", source);
+        fprintf(stderr, "usage: %s source destination\n", *argv);
+        exit(1);
     }
-    if ((out_fd = creat(destination, COPYMORE)) == -1)
+    char * source = argv[0];
+    char * destination = argv[1];
+    if(source[0] != '/' && destination[0] != '/')
+        files_copy(source, destination);
+    else if(source[0] != '/' && destination[0] == '/')
     {
-        oops("Cannot creat ", destination);
+        int i;
+        for (i = 1; i <= strlen(destination); i++)
+            destination[(i - 1)] = destination[i];
+        strcat(destination, "/");
+        strcat(destination, source);
+        files_copy(source, destination);
     }
-    /* copy files */
-    while ((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0)
+    else if(source[0] == '/' && destination[0] == '/')
     {
-        if (write(out_fd, buf, n_chars) != n_chars)
-        {
-            oops("Write error to ", destination);
-        }
-        if (n_chars == -1)
-        {
-            oops("Read error from ", source);
-        }
+        int i;
+        for(i = 1; i <= strlen(destination); i++)
+            destination[(i - 1)] = destination[i];
+        for(i = 1; i <= strlen(source); i++)
+            source[(i - 1)] = source[i];
+        directory_copy(source, destination);
     }
-    if (close(in_fd) == -1 || close(out_fd) == -1)
+    else
     {
-        oops("Error closing files", "");
+        fprintf(stderr, "usage: cp1 source destination\n");
+        exit(1);
     }
-    return 1;
-}
-void oops(char *s1, char *s2)
-{
-    fprintf(stderr, "Error: %s ", s1);
-    perror(s2);
-    exit(1);
 }
